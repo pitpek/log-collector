1ab5f169d3b6260fa964ce537d80cb67b7be4123
Daniil <d.federyakin@inbox.ru>
2024-06-30 22:58:16 +0300

    added consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"logcollector/internal/config"
+	"logcollector/internal/consumer"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalf("cmd/consumer/main.go: Failed to load config: %v", err)
+	}
+
+	cons := consumer.NewConsumer(cfg)
+	if err := cons.Start(); err != nil {
+		log.Fatalf("cmd/consumer/start.go: Failed to start")
+	}
+
+}
