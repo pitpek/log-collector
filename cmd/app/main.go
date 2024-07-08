08db5991a24fcdb798511d2d2f1dcd0c606f0099
Daniil <d.federyakin@inbox.ru>
2024-07-08 21:49:14 +0300

    added initialization of http server and client redis

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,40 +3,62 @@ package main
 import (
 	"context"
 	"log"
+	"logcollector/internal/api"
 	"logcollector/internal/config"
 	"logcollector/internal/consumer"
-	"logcollector/internal/storage"
+	"logcollector/internal/server"
+	"logcollector/internal/storage/postgres"
+	"logcollector/internal/storage/redis"
 	"logcollector/pkg/migrate"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
+	// Загрузка конфигов
 	cfg, err := config.LoadConfig("configs/config.yaml")
 	if err != nil {
 		log.Fatalf("cmd/app/main.go: Failed to load config: %v", err)
 	}
 
 	// Инициализация PostgreSQL
-	db, err := storage.NewPostgres(&cfg.Postgres)
+	db, err := postgres.NewPostgres(&cfg.Postgres)
 	if err != nil {
 		log.Fatalf("cmd/app/main.go: Failed to start postgres: %v", err)
 	}
 	defer db.Close()
 
+	// Проверка подключения к Postgres
 	err = db.Ping()
 	if err != nil {
 		log.Fatalf("cmd/app/main.go: Failed to ping postgres: %v", err)
 	}
 	log.Println("cmd/app/main.go: Database connected")
 
+	// Выполнение миграций
 	err = migrate.StartMigration(db.DB())
 	if err != nil {
 		log.Fatalf("cmd/app/main.go: Failed to run migrations: %v", err)
 	}
 	log.Println("cmd/app/main.go: Migrations applied successfully")
 
+	// Инициализация Redis
+	redisClient, err := redis.NewClient(&cfg.Redis)
+	if err != nil {
+		log.Fatalf("cmd/app/main.go: Failed to connect redis: %v", err)
+	}
+	defer redisClient.Close()
+
+	// Проверка подключения к redis
+	err = redisClient.Ping()
+	if err != nil {
+		log.Fatalf("cmd/app/main.go: Failed to ping Redis: %v", err)
+	}
+	log.Println("cmd/app/main.go: Redis connected successfully")
+
+	// Инициализация Consumer
 	cons := consumer.NewConsumer(cfg.Kafka.Brokers, cfg.Kafka.Topic, cfg.Kafka.Group, db)
 
 	ctx, cancel := context.WithCancel(context.Background())
@@ -47,10 +69,21 @@ func main() {
 			log.Fatalf("cmd/app/main.go: Failed to start consumer: %v", err)
 		}
 	}()
+	log.Println("cmd/app/main.go: Consumer started successfully")
 
+	// Запуск HTTP-сервера
+	serv := new(server.Server)
+	go func() {
+		if err := serv.Run(cfg.API.Port, api.InitRoutes()); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("cmd/app/main.go: Server start error: %v", err)
+		}
+	}()
+	log.Println("cmd/app/main.go: Server started successfully")
+
+	// Ожидание комманды завершения программы
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
 	<-sigCh
-	log.Println("cmd/app/main.go: shutting down app")
+	log.Println("cmd/app/main.go: Shutting down app")
 	cancel()
 }
