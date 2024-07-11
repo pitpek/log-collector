d2b3b9eac85876297ee17df0305374c2d8ec066b
Daniil Federyakin <d.federyakin@inbox.ru>
2024-07-11 04:42:43 +0300

    added producer implementation

---
+++ internal/producer/producer.go
@@ -0,0 +1,53 @@
+package producer
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type Producer struct {
+	writer *kafka.Writer
+}
+
+// NewProducer создает новый экземпляр Kafka producer
+// brokers - список брокеров Kafka
+// topic - тема Kafka, в которую нужно отправлять сообщения
+func NewProducer(brokers []string, topic string) *Producer {
+	return &Producer{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(brokers...),
+			Topic:    topic,
+			Balancer: &kafka.LeastBytes{},
+		},
+	}
+}
+
+// Start запускает процесс отправки сообщений в Kafka
+// ctx - контекст для управления жизненным циклом процесса
+func (p *Producer) Start(ctx context.Context, key string) error {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			// Здесь можно добавить логику для получения данных из ClickHouse или другого источника
+			messageValue := "some message" // Замените это на реальные данные
+
+			err := p.writer.WriteMessages(ctx, kafka.Message{
+				Key:   []byte(key),
+				Value: []byte(messageValue),
+			})
+			if err != nil {
+				log.Printf("internal/producer/producer.go: could not write message: %v", err)
+			} else {
+				log.Printf("internal/producer/producer.go: message sent: %s", messageValue)
+			}
+		}
+	}
+}
