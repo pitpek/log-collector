57340b18042f0cbf8efcac52b4c77ea277af72f2
Daniil Federyakin <d.federyakin@inbox.ru>
2024-07-12 15:59:00 +0300

    adder router to receive all logs

--- internal/api/router.go
+++ internal/api/router.go
@@ -2,12 +2,21 @@ package api
 
 import (
 	"logcollector/internal/monitoring"
+	"logcollector/internal/service"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Router struct {
+	service *service.Service
+}
+
+func NewRouter(service *service.Service) *Router {
+	return &Router{service: service}
+}
+
 // InitRoutes инициализирует маршруты для HTTP-сервера.
-func InitRoutes() *gin.Engine {
+func (r *Router) InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	// Добавление middleware для регистрации метрик
@@ -16,6 +25,11 @@ func InitRoutes() *gin.Engine {
 	metrics := router.Group("/metrics")
 	{
 		metrics.GET("/", gin.WrapH(prometheusHandler()))
+	}
+
+	logs := router.Group("/logs")
+	{
+		logs.GET("/", r.getLogs)
 	}
 
 	return router
